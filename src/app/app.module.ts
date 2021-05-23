f150a977c16919be6ce01a6adb67d65769764e3b
Yann Moskovitz <yann.moskovitz@hotmail.com>
2021-05-23 17:09:13 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/Icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatMenuModule} from '@angular/material/menu';
+import { FormsModule } from '@angular/forms';
+
+
+//NG8001: 'mat-form-field' is not a known element:
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableComponent } from './table/table.component';
+import { Routes } from '@angular/router';
+
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PaginationComponent } from './pagination/pagination.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+const routes: Routes = [
+  {
+      path: 'table',
+      component: TableComponent,
+  },
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    PaginationComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatMenuModule,
+    PaginationModule.forRoot(),
+    FormsModule,
+    MatPaginatorModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
